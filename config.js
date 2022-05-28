a836db968b3ec6ff8365ca11e91e5e800c77fb76
Kevork <ksayad@gmail.com>
2022-05-28 01:15:00 -0400

    config to ignore packages

--- config.js
+++ config.js
@@ -1,35 +1,54 @@
 module.exports = {
     gitAuthor: 'Renovator <noreply@kevvlvl.renovatebot.org>',
-    token: 'REPLACE_GITHUB_PAT_HERE',
+    token: '',
     platform: 'github',
     // executionTimeout: 60, // set max timeout of 60 minutes.
     // cacheDir: '',         // change where renovate's cache is stored.
     persistRepoData: true,   // repo data is kept between executions. git fetch will be used instead of git clone.
-    draftPr: true,
-    // dryRun: 'full',       // when activated, no PR is created. Obtain detailed log of what Renovate will do
+    dryRun: 'full',       // when activated, no PR is created. Obtain detailed log of what Renovate will do
     onboardingConfig: {
         extends: ['config:base'],
     },
     repositories: [
-
         {
-            // The PR's metadata (title, desc, etc.) can be customised using multiple configs: https://docs.renovatebot.com/configuration-options/#prtitle
             repository: 'kevvlvl/quarkus-carservice-api',
-            cloneSubmodules: true,                         // default = false. Make sure SSH auth is configured where RenovateBot is running
-            addLabels: 'Renovated by the Renovator!',      // Not be supported in BitBucket: https://docs.renovatebot.com/modules/platform/bitbucket-server/
-            additionalReviewers: 'team-leads@kevvlvl.github.com',
-            ignoreDeps: ['assertj-core', 'rest-assured'],
+            cloneSubmodules: true,
+            addLabels: 'Renovated by the Renovator!',
             packageRules: [
                 {
                     description: 'Enable Minor and Patch updates types',
                     matchUpdateTypes: ['minor', 'patch'],
                     enabled: true,
-                    labels: 'UPDATE-MINOR-PATCH'           // Not be supported in BitBucket: https://docs.renovatebot.com/modules/platform/bitbucket-server/
+                    labels: 'UPDATE-MINOR-PATCH'
                 },
-                // {   // This rule will tell renovatebot to ignore all packages starting with "rest" excluding rest-assured
-                //     matchPackagePatterns: ["^rest"],
-                //     excludePackageNames: ["rest-assured"]
-                // }
+            ],
+        },
+        {
+            repository: 'kevvlvl/ApiTestCucumber',
+            cloneSubmodules: true,
+            addLabels: 'Renovated by the Renovator!',
+            packageRules: [
+                {
+                    description: 'Enable Minor and Patch updates types',
+                    matchUpdateTypes: ['minor', 'patch'],
+                    enabled: true,
+                    labels: 'UPDATE-MINOR-PATCH',
+                    matchPackagePrefixes: [
+                        "com.fasterxml",
+                        "io.rest-assured",
+                        "org.hamcrest",
+                    ],
+                    excludePackagePrefixes: ["io.cucumber", "org.apache"]
+                },
+                {
+                    matchUpdateTypes: ['minor', 'patch'],
+                    enabled: false,
+                    labels: 'UPDATE-MINOR-PATCH',
+                    matchPackagePrefixes: [
+                        "io.cucumber",
+                        "org.apache"
+                    ],
+                },
             ],
         },
     ],
